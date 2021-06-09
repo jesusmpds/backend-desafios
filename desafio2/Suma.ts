6b7441ae94fe56d75cb77342dc7c29128a1cc09f
Jesus Perez <jesusmpds18@gmail.com>
2021-06-09 12:41:35 -0300

    Added private variable

--- desafio2/Suma.ts
+++ desafio2/Suma.ts
@@ -1,6 +1,6 @@
 export class Suma {
-    number1: number;
-    number2: number;
+    private number1: number;
+    private number2: number;
     constructor(number1: number,number2: number){
     this.number1 = number1;
     this.number2 = number2;
